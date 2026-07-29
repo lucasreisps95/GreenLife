a121169933d3a508d1b171df4dc0364fc12685cd
dehil <dehilee@gmail.com>
2026-07-28 19:19:46 -0700

    Rebuild owner dashboard with React and Tailwind

---
+++ owner-dashboard/tailwind.config.js
@@ -0,0 +1,5 @@
+export default {
+  content: ['./app/**/*.{html,js,jsx}'],
+  theme: { extend: { boxShadow: { card: '0 1px 2px rgb(15 23 42 / .04), 0 12px 30px rgb(15 23 42 / .05)' } } },
+  plugins: []
+}
